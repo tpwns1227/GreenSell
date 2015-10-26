da9a165671d22f0a933ddb588fafb65d0cad335d
RAPA01 <RAPA01@RAPA01-PC>
2015-10-26 18:50:25 +0900

    게시판 Bean 생성

---
+++ GreenSell/src/com/greensell/beans/BbsVo.java
@@ -0,0 +1,58 @@
+package com.greensell.beans;
+
+import java.sql.Date;
+
+public class BbsVo {
+
+	private int no;//게시글 번호
+	private int bbsno;//게시판 분류 번호
+	private String title;//게시글제목
+	private Date bbsdate;//글쓴 날짜
+	private String content;//게시글내용
+	private String email;//작성자
+	private int hits;//조회수	
+	
+	public int getNo() {
+		return no;
+	}
+	public void setNo(int no) {
+		this.no = no;
+	}
+	public int getBbsno() {
+		return bbsno;
+	}
+	public void setBbsno(int bbsno) {
+		this.bbsno = bbsno;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public Date getBbsdate() {
+		return bbsdate;
+	}
+	public void setBbsdate(Date bbsdate) {
+		this.bbsdate = bbsdate;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public int getHits() {
+		return hits;
+	}
+	public void setHits(int hits) {
+		this.hits = hits;
+	}
+	
+}
---
+++ GreenSell/src/com/greensell/beans/ReplyVo.java
@@ -0,0 +1,42 @@
+package com.greensell.beans;
+
+import java.sql.Date;
+
+public class ReplyVo {
+	private int cmno;//댓글 번호
+	private int no;//글번호
+	private String email;//작성자
+	private Date cmdate;//댓글단 시간
+	private String cmcontent;//댓글내용
+	
+	public int getCmno() {
+		return cmno;
+	}
+	public void setCmno(int cmno) {
+		this.cmno = cmno;
+	}
+	public int getNo() {
+		return no;
+	}
+	public void setNo(int no) {
+		this.no = no;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Date getCmdate() {
+		return cmdate;
+	}
+	public void setCmdate(Date cmdate) {
+		this.cmdate = cmdate;
+	}
+	public String getCmcontent() {
+		return cmcontent;
+	}
+	public void setCmcontent(String cmcontent) {
+		this.cmcontent = cmcontent;
+	}
+}
