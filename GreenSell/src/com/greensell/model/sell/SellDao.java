ddfbfbf7ae31ba5acec2c702ddb9a0da0f34fb57
tpwns1227 <tpwns1227@naver.com>
2015-10-27 17:00:39 +0900

    DAO 인터페이스 주석 수정

--- GreenSell/src/com/greensell/model/sell/SellDao.java
+++ GreenSell/src/com/greensell/model/sell/SellDao.java
@@ -8,19 +8,19 @@ import com.greensell.sell.beans.ItemSellVO;
 
 public interface SellDao {
 	
-	public List<ItemSellVO> allitemList() throws SQLException; //��ü ��� �Ѹ���  
+	public List<ItemSellVO> allitemList() throws SQLException; //모든 판매글 리스트 출력  
 	
-	public List<ItemSellVO> olditemList(String howsell) throws SQLException; //�߰� ����Ʈ
+	public List<ItemSellVO> olditemList(String howsell) throws SQLException; //중고물품 리스트 출력
 	
-	public List<AuctionVO> auctionitemList() throws SQLException; //��� ����Ʈ
+	public List<AuctionVO> auctionitemList() throws SQLException; //경매물품 리스트 출력
 	
-	public List<String> getImagenames(int no) throws SQLException; //��ǰ �̹��� �̸� ��������
+	public List<String> getImagenames(int no) throws SQLException; //이미지의 이름을 가져오기
 	
-	public boolean itemInsert(ItemSellVO itsv) throws SQLException; //��ǰ �߰��ϱ�
+	public boolean itemInsert(ItemSellVO itsv) throws SQLException; //물품 등록
 	
-	public List<ItemSellVO> itemDetail(int no) throws SQLException;//��ǰ �󼼺���
+	public List<ItemSellVO> itemDetail(int no) throws SQLException;//물품 상세보기
 	
-	public boolean itemUpdate(int no) throws SQLException; //��ǰ �Խñ� ������Ʈ
+	public boolean itemUpdate(int no) throws SQLException; //물품 글 업데이트
 	
-	public boolean itemDelete(int no) throws SQLException;//��ǰ �Խñ� ����
+	public boolean itemDelete(int no) throws SQLException;//물품 게시글 삭제
 }
