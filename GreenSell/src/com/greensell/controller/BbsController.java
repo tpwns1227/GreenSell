14b0f25ee05cccb835a65b98dfeefd1cbefb0ccb
Randomo <RAPA01@RAPA01-PC>
2015-10-27 13:41:59 +0900

    BbsConroller

--- GreenSell/src/com/greensell/controller/BbsController.java
+++ GreenSell/src/com/greensell/controller/BbsController.java
@@ -1,10 +1,20 @@
 package com.greensell.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.greensell.model.bbs.BbsDao;
 
 @Controller
 public class BbsController {
 
-	
+	@Autowired
+	BbsDao dao;
+	//게시글 쓰기
+	@RequestMapping("/write")
+	public String write(){
+		return "bbs/BbsWrite";
+	}
 	
 }
