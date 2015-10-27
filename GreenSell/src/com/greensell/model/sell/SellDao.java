52a526a8ed12eec85d20e2c379c3bda151de7b95
Joshep <Joshep@Be-pious>
2015-10-27 20:16:40 +0900

    update 추가

--- GreenSell/src/com/greensell/model/sell/SellDao.java
+++ GreenSell/src/com/greensell/model/sell/SellDao.java
@@ -10,22 +10,21 @@ import com.greensell.sell.beans.ItemSellVO;
 
 public interface SellDao {
 	
-	public List<ItemSellVO> allitemList() throws SQLException; //모든 판매글 리스트 출력  
+	public List<ItemSellVO> allitemList() throws SQLException; //紐⑤�� ��留ㅺ� 由ъ�ㅽ�� 異���  
 	
-	public List<ItemSellVO> olditemList(String howsell) throws SQLException; //중고물품 리스트 출력
+	public List<ItemSellVO> olditemList(String howsell) throws SQLException; //以�怨�臾쇳�� 由ъ�ㅽ�� 異���
 	
-	public List<AuctionVO> auctionitemList() throws SQLException; //경매물품 리스트 출력
+	public List<AuctionVO> auctionitemList() throws SQLException; //寃쎈ℓ臾쇳�� 由ъ�ㅽ�� 異���
 	
-	public List<String> getImagenames(int no) throws SQLException; //이미지의 이름을 가져오기
+	public List<String> getImagenames(int no) throws SQLException; //�대�몄��� �대��� 媛��몄�ㅺ린
 	
-	public boolean itemInsert(ItemSellVO itsv) throws SQLException; //물품 등록
+	public boolean itemInsert(ItemSellVO itsv) throws SQLException; //臾쇳�� �깅�
 	
-	public List<ItemSellVO> itemDetail(int no) throws SQLException;//물품 상세보기
+	public List<ItemSellVO> itemDetail(int no) throws SQLException;//臾쇳�� ���몃낫湲�
 	
-	public boolean itemUpdate(int no) throws SQLException; //물품 글 업데이트
+	public boolean itemUpdate(ItemSellVO itsv) throws SQLException; //臾쇳�� 湲� ���곗�댄��
 	
-	public boolean itemDelete(int no) throws SQLException;//물품 게시글 삭제
+	public boolean itemDelete(int no) throws SQLException;//臾쇳�� 寃���湲� ����
 	
-	public boolean auctionDelete(int no) throws SQLException;//경매일 경우 삭제 
-	
+	public boolean auctionDelete(int no) throws SQLException;//寃쎈ℓ�� 寃쎌�� ���� 
 }
--- GreenSell/src/com/greensell/model/sell/SellDaoImpl.java
+++ GreenSell/src/com/greensell/model/sell/SellDaoImpl.java
@@ -11,73 +11,74 @@ import com.greensell.sell.beans.ItemSellVO;
 
 public class SellDaoImpl implements SellDao {
 
-	@Autowired
-	private SqlSession sqlSession;
-	 
+   @Autowired
+   private SqlSession sqlSession;
+    
 
-	@Override
-	public List<ItemSellVO> allitemList() throws SQLException { //전체 item리스트 출력
-		// TODO Auto-generated method stub
-		List<ItemSellVO> list = sqlSession.selectList("selectAll");
-		return list;
-	}
+   @Override
+   public List<ItemSellVO> allitemList() throws SQLException { //전체 item리스트 출력
+      // TODO Auto-generated method stub
+      List<ItemSellVO> list = sqlSession.selectList("selectAll");
+      return list;
+   }
 
-	@Override
-	public List<ItemSellVO> olditemList(String howsell) throws SQLException { //중고 itemlist출력
-		// TODO Auto-generated method stub
-		List<ItemSellVO> list;
-		if(howsell==null){
-			howsell = "중고";
-		}
-		list = sqlSession.selectList("selectolditem", howsell);
-		return list;
-	}
+   @Override
+   public List<ItemSellVO> olditemList(String howsell) throws SQLException { //중고 itemlist출력
+      // TODO Auto-generated method stub
+      List<ItemSellVO> list;
+      if(howsell==null){
+         howsell = "중고";
+      }
+      list = sqlSession.selectList("selectolditem", howsell);
+      return list;
+   }
 
-	@Override
-	public List<AuctionVO> auctionitemList() throws SQLException { //경매 itemList출력
-		// TODO Auto-generated method stub
-		List<AuctionVO>	list = sqlSession.selectList("selectauctionitem");
-			return list;
-	}
+   @Override
+   public List<AuctionVO> auctionitemList() throws SQLException { //경매 itemList출력
+      // TODO Auto-generated method stub
+      List<AuctionVO>   list = sqlSession.selectList("selectauctionitem");
+         return list;
+   }
 
-	@Override
-	public List<String> getImagenames(int no) throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
-	}
+   @Override
+   public List<String> getImagenames(int no) throws SQLException {
+      // TODO Auto-generated method stub
+      return null;
+   }
 
-	@Override
-	public boolean itemInsert(ItemSellVO itsv) throws SQLException {
-		// TODO Auto-generated method stub
-		return false;
-	}
+   @Override
+   public boolean itemInsert(ItemSellVO itsv) throws SQLException {
+      // TODO Auto-generated method stub
+      return false;
+   }
 
-	@Override
-	public List<ItemSellVO> itemDetail(int no) throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
-	}
+   @Override
+   public List<ItemSellVO> itemDetail(int no) throws SQLException {
+      // TODO Auto-generated method stub
+      return null;
+   }
 
-	@Override
-	public boolean itemUpdate(int no) throws SQLException {
-		// TODO Auto-generated method stub
-		return false;
-	}
+   @Override
+   public boolean itemUpdate(ItemSellVO itsv) throws SQLException {
+      // TODO Auto-generated method stub
+	   int i =	sqlSession.update("sell.update",itsv);
+	   return (i>0)? true:false;
+   }
 
-	@Override
-	public boolean itemDelete(int no) throws SQLException {
-		// TODO Auto-generated method stub
-		int i = sqlSession.delete("deleteitem", no);
-		 return (i>0)? true:false;
-	}
+   @Override
+   public boolean itemDelete(int no) throws SQLException {
+      // TODO Auto-generated method stub
+      int i = sqlSession.delete("deleteitem", no);
+       return (i>0)? true:false;
+   }
 
-	@Override
-	public boolean auctionDelete(int no) throws SQLException {
-		// TODO Auto-generated method stub
-		int i = sqlSession.delete("deleteauction", no);
-		return (i>0)? true:false;
-	}
+   @Override
+   public boolean auctionDelete(int no) throws SQLException {
+      // TODO Auto-generated method stub
+      int i = sqlSession.delete("deleteauction", no);
+      return (i>0)? true:false;
+   }
 
-	
-	
+   
+   
 }
