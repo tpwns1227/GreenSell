9f5a35245f2f8deeb1b35119b5e5e418856dce6e
tpwns1227 <tpwns1227@naver.com>
2015-10-27 14:15:00 +0900

    sell Dao 기능 정의

--- GreenSell/src/com/greensell/model/sell/SellDao.java
+++ GreenSell/src/com/greensell/model/sell/SellDao.java
@@ -1,9 +1,26 @@
 package com.greensell.model.sell;
 
+import java.sql.SQLException;
 import java.util.List;
 
+import com.greensell.sell.beans.AuctionVO;
+import com.greensell.sell.beans.ItemSellVO;
+
 public interface SellDao {
-	public String personList();
 	
+	public List<ItemSellVO> allitemList() throws SQLException; //전체 목록 뿌리기 
 	
+	public List<ItemSellVO> olditemList(String category) throws SQLException; //중고 리스트
+	
+	public List<AuctionVO> auctionitemList(String category) throws SQLException; //경매 리스트
+	
+	public List<String> getImagenames(int no) throws SQLException; //상품 이미지 이름 가져오기
+	
+	public boolean itemInsert(ItemSellVO itsv) throws SQLException; //상품 추가하기
+	
+	public List<ItemSellVO> itemDetail(int no) throws SQLException;//상품 상세보기
+	
+	public boolean itemUpdate(int no) throws SQLException; //상품 게시글 업데이트
+	
+	public boolean itemDelete(int no) throws SQLException;//상품 게시글 삭제
 }
--- GreenSell/src/com/greensell/model/sell/SellDaoImpl.java
+++ GreenSell/src/com/greensell/model/sell/SellDaoImpl.java
@@ -1,18 +1,66 @@
 package com.greensell.model.sell;
 
+import java.sql.SQLException;
+import java.util.List;
+
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import com.greensell.sell.beans.AuctionVO;
+import com.greensell.sell.beans.ItemSellVO;
 
 public class SellDaoImpl implements SellDao {
 
 	@Autowired
 	private SqlSession sqlSession;
 	
+
 	@Override
-	public String personList() {
+	public List<ItemSellVO> allitemList() throws SQLException {
 		// TODO Auto-generated method stub
-		String p = sqlSession.selectOne("emp.empname");
-		return p;
+		return null;
+	}
+
+	@Override
+	public List<ItemSellVO> olditemList(String category) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<AuctionVO> auctionitemList(String category) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<String> getImagenames(int no) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean itemInsert(ItemSellVO itsv) throws SQLException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public List<ItemSellVO> itemDetail(int no) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean itemUpdate(int no) throws SQLException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean itemDelete(int no) throws SQLException {
+		// TODO Auto-generated method stub
+		return false;
 	}
 
 	
