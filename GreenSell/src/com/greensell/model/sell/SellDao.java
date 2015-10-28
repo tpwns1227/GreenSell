97701bc661950bcc0e6665a51de39fc1fe680bf9
tpwns1227 <tpwns1227@naver.com>
2015-10-28 14:14:37 +0900

    주석 수정

--- GreenSell/src/com/greensell/model/sell/SellDao.java
+++ GreenSell/src/com/greensell/model/sell/SellDao.java
@@ -10,21 +10,13 @@ import com.greensell.sell.beans.ItemSellVO;
 
 public interface SellDao {
 	
-	public List<ItemSellVO> allitemList() throws SQLException; //紐⑤�� ��留ㅺ� 由ъ�ㅽ�� 異���  
-	
-	public List<ItemSellVO> olditemList(String howsell) throws SQLException; //以�怨�臾쇳�� 由ъ�ㅽ�� 異���
-	
-	public List<AuctionVO> auctionitemList() throws SQLException; //寃쎈ℓ臾쇳�� 由ъ�ㅽ�� 異���
-	
-	public List<String> getImagenames(int no) throws SQLException; //�대�몄��� �대��� 媛��몄�ㅺ린
-	
-	public boolean itemInsert(ItemSellVO itsv) throws SQLException; //臾쇳�� �깅�
-	
-	public List<ItemSellVO> itemDetail(int no) throws SQLException;//臾쇳�� ���몃낫湲�
-	
-	public boolean itemUpdate(ItemSellVO itsv) throws SQLException; //臾쇳�� 湲� ���곗�댄��
-	
-	public boolean itemDelete(int no) throws SQLException;//臾쇳�� 寃���湲� ����
-	
-	public boolean auctionDelete(int no) throws SQLException;//寃쎈ℓ�� 寃쎌�� ���� 
+	public List<ItemSellVO> allitemList() throws SQLException; //홈에서 중고 및 경매 리스트 통합 출력
+	public List<ItemSellVO> olditemList(String howsell) throws SQLException; //중고 판매 리스트 출력
+	public List<AuctionVO> auctionitemList() throws SQLException; //경매 리스트 출력
+	public List<String> getImagenames(int no) throws SQLException; //게시글에 포함된 이미지 이름들 가져오기
+	public boolean itemInsert(ItemSellVO itsv) throws SQLException; //판매 게시글 올리기
+	public List<ItemSellVO> itemDetail(int no) throws SQLException;//판매글 상세보기	
+	public boolean itemUpdate(ItemSellVO itsv) throws SQLException; //중고 게시물 수정	
+	public boolean itemDelete(int no) throws SQLException;//중고 물품 및 경매 게시물 글 삭제	
+	public boolean auctionDelete(int no) throws SQLException;//경매 게시물 글 삭제
 }
