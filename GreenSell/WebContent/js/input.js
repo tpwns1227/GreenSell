4542ecf7e2f5cfe6ee8d436d68d74893b52da221
tpwns1227 <tpwns1227@naver.com>
2015-10-29 19:20:54 +0900

    커밋

--- GreenSell/WebContent/js/input.js
+++ GreenSell/WebContent/js/input.js
@@ -9,22 +9,33 @@ $(document).ready(function() {
 		}, 500, function() {
 			$("input[value='다음']").blur();
 		});
-	});
+	
+	if (document.sellForm.howsell.value == '경매') {
+		$("#auction").text('경매 시작가격');
+	} else {
+		$("#auction").text('판매가격');
+	}
 
-	$("#type").click(function() {
-		if (document.sellForm.howsell.value == '경매') {
-			$("#auction").html('시작가격');
-		} else {
-			$("#auction").html('판매가격');
-		}
 	});
+	
+
+		   $(".calen").datepicker({
+				   nextText: '▶',
+				   prevText: '◀',  
+			      dateFormat:"yy-mm-dd",
+			      dayNamesMin:['월','화','수','목','금','토','일'],
+			      showButtonPanel:true,
+			      closeText: '닫기'
+		   });
+	
+	
 
 	$("input[value='이전']").click(function() {
 		t += 600;
 		$("#slidebox").animate({
 			top : t
 		}, 500, function() {
-			$("input[value='다음']").blur();
+			$("input[value='이전']").blur();
 		});
 	});
 
@@ -32,17 +43,17 @@ $(document).ready(function() {
 
 function ch() {
 	document.querySelector('.img1').innerHTML = document.querySelector('#img1').value;
-	document.getElementById('image2').innerHTML = "<input type='file' id='img2' onchange='ch2()'><label class='img2' for='img2'>클릭</label>";
+	document.getElementById('image2').innerHTML = "<input type='file' id='img2' onchange='ch2()' name='imgname2'><label class='img2' for='img2'>클릭</label>";
 };
 
 function ch2() {
 	document.querySelector('.img2').innerHTML = document.querySelector('#img2').value;
-	document.getElementById('image3').innerHTML = "<input type='file' id='img3' onchange='ch3()'><label class='img3' for='img3'>클릭</label>";
+	document.getElementById('image3').innerHTML = "<input type='file' id='img3' onchange='ch3()' name='imgname3'><label class='img3' for='img3'>클릭</label>";
 };
 
 function ch3() {
 	document.querySelector('.img3').innerHTML = document.querySelector('#img3').value;
-	document.getElementById('image4').innerHTML = "<input type='file' id='img4' onchange='ch4()'><label class='img4' for='img4'>클릭</label>";
+	document.getElementById('image4').innerHTML = "<input type='file' id='img4' onchange='ch4()' name='imgname4'><label class='img4' for='img4'>클릭</label>";
 };
 
 function ch4() {
