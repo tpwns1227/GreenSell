2ecac7c00c4f9733ffadbf285070ff9e5dcd09a7
jskim7067 <jskim7067@naver.com>
2015-10-29 22:03:52 +0900

    zipvo

---
+++ GreenSell/src/com/greensell/member/beans/ZipVo.java
@@ -0,0 +1,42 @@
+package com.greensell.member.beans;
+
+public class ZipVo {
+	
+	String zipcode;
+	String sido;
+	String gugan;
+	String dong;
+	String bunji;
+	public String getZipcode() {
+		return zipcode;
+	}
+	public void setZipcode(String zipcode) {
+		this.zipcode = zipcode;
+	}
+	public String getSido() {
+		return sido;
+	}
+	public void setSido(String sido) {
+		this.sido = sido;
+	}
+	public String getGugan() {
+		return gugan;
+	}
+	public void setGugan(String gugan) {
+		this.gugan = gugan;
+	}
+	public String getDong() {
+		return dong;
+	}
+	public void setDong(String dong) {
+		this.dong = dong;
+	}
+	public String getBunji() {
+		return bunji;
+	}
+	public void setBunji(String bunji) {
+		this.bunji = bunji;
+	}
+	
+	
+}
