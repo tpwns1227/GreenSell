0b68d8d86c1a31740de95a110b7470c5ccab4b11
tpwns1227 <tpwns1227@naver.com>
2015-10-30 16:50:26 +0900

    수정 & 삭제 추가

--- GreenSell/src/com/greensell/controller/SellController.java
+++ GreenSell/src/com/greensell/controller/SellController.java
@@ -85,24 +85,32 @@ public class SellController {
 	}
 
 	// UTF
-	@RequestMapping("deleteitem") // delete 기능구현
-	public String deleteitem(@RequestParam int no, @RequestParam String howsell) throws SQLException {
-		boolean r = false;
-		if (howsell.equals("경매")) {
+	@RequestMapping("/deleteitem") // delete 기능구현
+	public String deleteitem(@RequestParam int no) throws SQLException {
+		//boolean r = false;
+		/*if (howsell.equals("경매")) {
 			 r = dao.auctionDelete(no);
+		}*/
+		
+		if (dao.itemDeleteimg(no)) {
+			System.out.println("이미지 삭제에 성공하셨습니다.");
 		}
-		if (r && dao.itemDelete(no)) {
-			System.out.println("삭제에 성공하셨습니다.");
+		
+		if (dao.itemDelete(no)) {
+			System.out.println("게시글 삭제에 성공하셨습니다.");
 		} else {
 			System.out.println("삭제에 실패하였습니다.");
 		}
 		return "/sell/main";
 	}
 
-	@RequestMapping("/updateitem")//자신이 올린글 수정
-	public String updateitem(@RequestParam ItemSellVO iso) throws SQLException { 
-	
-		return "1023/update_form";
+	@RequestMapping("/updateitem_form")//자신이 올린글 수정
+	public String updateitem(@RequestParam int no, Model m) throws SQLException { 
+		
+		ItemSellVO ivo = dao.itemDetail(no); // 게시글 한가지의 정보를 가져온다.
+		m.addAttribute("itemone", ivo);
+		
+		return "/sell/update_form";
 	}
 
 	@RequestMapping("/inputform")//판매하기 눌렀을때 판매로 이동
