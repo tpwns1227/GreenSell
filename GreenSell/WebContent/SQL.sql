81237f3a8f641c1da1beb798624328bd40a7afd9
tpwns1227 <tpwns1227@naver.com>
2015-11-02 18:22:14 +0900

    dsffdsdf

--- GreenSell/WebContent/SQL.sql
+++ GreenSell/WebContent/SQL.sql
@@ -1,55 +1,55 @@
-
-create table Member( -- ȸ�� ���̺�
+create table Member( 
    email varchar2(50) primary key not null,
    password varchar2(50) not null,
    name varchar2(20) not null,
    nickname varchar2(30) not null,
    phone varchar2(20) not null,
-   address varchar2(100) not null,
-   bank varchar2(15) not null,
+   zipcode varchar2(10) not null,
+   address varchar2(200) not null,
+   bank varchar2(20) not null,
    account varchar2(20) not null,
-   question varchar2(60) not null,
-   answer varchar2(60) not null,
+   question varchar2(50) not null,
+   answer varchar2(50) not null,
    point number not null,
    grade number not null,
    report number not null,
    joindate date not null
 );
-select count(no) from bbs where bbsno=1
-update member set password=123, bank=1234, address='세준팬티'
-where password=1234 and email='tpwns1227'
-insert into member values('tpwns1227','1234','윤세준','세준','010','세준0','세준','217053-56-124278','윤세준?','세준',0,0,0,SYSDATE)
-select * from member;
+--select count(no) from bbs where bbsno=1
+--update member set password=123, bank=1234, address='세준팬티'
+--where password=1234 and email='tpwns1227'
+--insert into member values('tpwns1227','1234','윤세준','세준','010','세준0','세준','217053-56-124278','윤세준?','세준',0,0,0,SYSDATE)
+--select * from member;
 
 create table bbsSort( --�Խ��� ����
    bbsno number primary key not null,
    bbsname varchar2(50) not null,
    bbscm char not null
 );
-insert into BBSSORT values (1,'��������','f')
-select * from BBSSORT
-select * from replycomment
+--insert into BBSSORT values (1,'��������','f')
+--select * from BBSSORT
+--select * from replycomment
 
 create table BBS( --�Խ��� 
 no number primary key not null,
-title varchar2(100) not null,
+title varchar2(150) not null,
 bbscontent varchar2(3000) not null, 
 email varchar2(50) not null,
 bbsdate date not null,
 hits number not null,
 bbsno number not null,
-foreign key (email) references member(email),
-foreign key (bbsno) references bbsSort(bbsno)
+foreign key (email) references member(email) on delete cascade,
+foreign key (bbsno) references bbsSort(bbsno) on delete cascade
 );
 
-create table replycomment( -- ���
+create table replycomment(
    cmno number primary key not null,
    no number not null,
    email varchar2(50) not null,
    cmdate date not null,
    cmcontent varchar2(200) not null,
-   foreign key (no) references bbs(no),
-   foreign key (email) references member(email)
+   foreign key (no) references bbs(no) on delete cascade,
+   foreign key (email) references member(email) on delete cascade
 );
 
 create table postscript( --�ı�
@@ -59,8 +59,8 @@ create table postscript( --�ı�
    content varchar2(2000) not null,
    point number not null,
    wemail varchar2(50) not null,
-   foreign key (email) references member(email),
-   foreign key (wemail) references member(email)
+   foreign key (email) references member(email) on delete cascade,
+   foreign key (wemail) references member(email) on delete cascade
 );
 
 create table point( 
@@ -70,26 +70,26 @@ create table point(
    price number not null,
    sort char not null,
    commission number not null,
-   foreign key (email) references member(email)
+   foreign key (email) references member(email) on delete cascade
 );
 
-create table itemsell( -- ��ǰ�Ǹ�
+create table itemsell(
    no number primary key not null,
-   itemname varchar2(20) not null,
+   itemname varchar2(200) not null,
    itemprice number not null,
-   itemstate varchar2(30) not null,
-   category varchar2(30) not null,
-   howsell varchar2(12) not null,
-   itemdetail varchar2(3000) not null,
+   itemstate varchar2(20) not null,
+   category varchar2(20) not null,
+   howsell varchar2(10) not null,
+   itemdetail varchar2(4000) not null,
    email varchar2(50) not null,
-   foreign key(email) REFERENCES member(email)
+   foreign key(email) REFERENCES member(email) on delete cascade
 );
 
 create table itemImg( -- 
 no number primary key not null,
 imgno number not null,
-imgname varchar2(100) not null,
-foreign key(imgno) REFERENCES itemsell(no)
+imgname varchar2(200) not null,
+foreign key(imgno) REFERENCES itemsell(no) on delete cascade
 );
 
 create table auction( --���
@@ -100,25 +100,41 @@ create table auction( --���
    finishtime date not null,
    tendernumber number not null,
    nowemail varchar2(50) not null,
-   foreign key(nowemail) REFERENCES member(email),
-   foreign key(itemno) REFERENCES itemsell(no)
+   foreign key(nowemail) REFERENCES member(email) on delete cascade,
+   foreign key(itemno) REFERENCES itemsell(no) on delete cascade
 );
 
 create table selected( -- ����
-   no number primary key,
+   selectno number primary key,
    itemno number not null,
    email varchar2(50) not null,
-   foreign key(itemno) REFERENCES itemsell(no),
-   foreign key(email) REFERENCES member(email)
-); 
+   foreign key(itemno) REFERENCES itemsell(no) on delete cascade,
+   foreign key(email) REFERENCES member(email) on delete cascade
+);
+
+select * from selected
 
 create table buy( --���Ÿ��
    no number primary key,
    itemno number not null,
    email varchar2(50) not null,
-   foreign key(itemno) REFERENCES itemsell(no),
-   foreign key(email) REFERENCES member(email)
+   foreign key(itemno) REFERENCES itemsell(no) on delete cascade,
+   foreign key(email) REFERENCES member(email) on delete cascade
 );
+
+
+
+drop table buy;
+drop table member;
+drop table itemsell;
+drop table itemimg;
+drop table auction;
+drop table selected;
+drop table bbs;
+drop table bbssort;
+drop table replycomment;
+drop table postscript;
+drop table point;
 
 
 
@@ -131,28 +147,3 @@ create sequence no_itemimg_seq start with 1 increment by 1;
 create sequence no_auction_seq start with 1 increment by 1;
 create sequence no_selected_seq start with 1 increment by 1;
 create sequence no_buy_seq start with 1 increment by 1;
-
-select * from zipcode;
-
-alter table bbs drop column bbscontent ;
-alter table bbs add (bbscontent varchar2(3000));
-
-alter table replycomment drop column cmcontent ;
-alter table replycomment add (cmcontent varchar2(200));
-
-
-insert into BBSSORT values (4,'공지사항','0');
-insert into BBSSORT values (2,'자주 묻는 질문','0');
-insert into BBSSORT values (3,'질문하기','1');
-
-
-
-
-
-
-
-
-
-
-
-
